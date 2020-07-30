36fb3e81a4b6ce5ea9e7e38e965b4176c751059b
Koksal Ogec <ogeckoksal@gmail.com>
2020-07-29 20:11:08 -0400

    Hooks class is created

---
+++ src/test/java/com/bitrix24/step_definitons/Hooks.java
@@ -0,0 +1,18 @@
+package com.bitrix24.step_definitons;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class Hooks {
+
+    @Before
+    public void setUp(){
+
+    }
+
+    @After
+    public void tearDown(){
+
+    }
+
+}
